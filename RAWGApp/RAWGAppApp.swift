6287e81fa21240d6791822d73ee0781847a8f848
Naufal Abiyyu <naufal1068@gmail.com>
2023-11-09 09:28:47 +0800

    Initial Commit

---
+++ RAWGApp/RAWGAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RAWGAppApp.swift
+//  RAWGApp
+//
+//  Created by Naufal Abiyyu on 09/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RAWGAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
